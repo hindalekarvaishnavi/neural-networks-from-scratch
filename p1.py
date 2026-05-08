32a981f8d65bd5a557cdec5c0536053cc5d61f56
hindalekarvaishnavi <hindalekar.vaishnavi@icloud.com>
2026-05-08 22:03:30 +0530

    layers, batches and objects in nn

--- p1.py
+++ p1.py
@@ -1,23 +1,21 @@
 import numpy as np
 
-inputs = [[1,2,3,2.5],
-          [2,3,1,0.5],
-          [1,1,1,1]]
+np.random.seed(0)
 
-weights1 = [[3.1,2.0,-0.5,1],
-           [0.5,-0.91,0.26,-0.5],
-           [-0.26,2.6,1.42,-0.12]]
+X = [[1,2,3,2.5],
+     [2,3,1,0.5],
+     [1,1,1,1]]
 
-biases1 = [2,3,0.5]
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
+    
+layer1 = Layer_Dense(4,3)
+layer2 = Layer_Dense(3,2)
 
-weights2 = [[0.1,-0.14,0.3],
-           [0.3,-0.7,1.6],
-           [-1.2,1.5,0]]
-
-biases2 = [-1,2,-0.5]
-
-layer1_output = np.dot(inputs,np.array(weights1).T) +biases1
-print(layer1_output)
-
-layer2_output = np.dot(layer1_output,np.array(weights2).T) +biases2
-print(layer2_output)
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
