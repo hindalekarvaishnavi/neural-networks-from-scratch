86aec706e57dc082946962d5c3eaa77daa35676b
hindalekarvaishnavi <hindalekar.vaishnavi@icloud.com>
2026-05-09 16:08:54 +0530

    adding softmax activation function

---
+++ p3.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from datasets import spiral_data
+
+X, y = spiral_data(100, 3)
+
+np.random.seed(0)
+
+# X = [[1,2,3,2.5],
+#      [2,3,1,0.5],
+#      [-1.5,2.7,3.3,-0.8]]
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
+    
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = norm_values
+        
+
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+
+layer2 = Layer_Dense(5,3)
+activation2 = Activation_Softmax()
+
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
+
+print(activation2.output)
